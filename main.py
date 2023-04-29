ca626ec5e2a3cd5b59782a37fe6f5c6abeffc9d3
Nurlan1511 <132122782+Nurlan1511@users.noreply.github.com>
2023-04-29 10:59:14 +0500

    Add files via upload
    дз на тему dictionary

---
+++ main.py
@@ -0,0 +1,16 @@
+products={"table" : "стол",
+          "school" : "школа",
+          "apple" : "яблоко",
+          "phone" : "телефон",
+          }
+print(products)
+print("---------------------------------------------")
+del products ["school"]
+print(products)
+print("---------------------------------------------")
+
+products["phone"]=["abrakataba"]
+print(products)
+print("---------------------------------------------")
+products["smarphone"] = "смартфон"
+print(products)
